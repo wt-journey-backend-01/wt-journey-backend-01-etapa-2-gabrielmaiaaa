c028105ce761cf4c62fbda64c7f8a880bceb2b9c
Gabriel Maia <gabrielmaia6743@gmail.com>
2025-09-02 17:47:41 -0300

    voltando para versão antiga

--- controllers/casosController.js
+++ controllers/casosController.js
@@ -1,245 +1,202 @@
 const casosRepository = require("../repositories/casosRepository");
 const agentesRepository = require("../repositories/agentesRepository");
 const errorHandler = require("../utils/errorHandler");
-const { ApiError } = require("../utils/errorHandler");
-const { dadosAgentes, dadosParcialAgentes, agenteIdValido, agenteCargoESorteValido } = require('../utils/agenteValidacao');
-const { z } = require('zod');
 
-function listarPorAgente(res, agente_id, next) {
+function listarPorAgente(res, agente_id) {
     if (!agentesRepository.encontrarAgenteById(agente_id)) {
-        return next(new ApiError(404, "ID do agente informado não encontrado no sistema."));
+        return res.status(404).json(errorHandler.handleError(404, "ID do agente informado não encontrado no sistema.", "agenteNaoEncontrado", "ID do agente informado não encontrado no sistema."));
     }
 
     const dados = casosRepository.listarCasosPorAgente(agente_id);
 
     if (!dados || dados.length === 0) {
-        return next(new ApiError(404, "Caso não encontrado com esse id de agente"));
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado com esse id de agente", "casoNaoEncontrado", "Caso não encontrado com esse id de agente"));
     }
 
     return res.status(200).json(dados);
 }
 
-function listarPorStatus(res, status, next) {
+function listarPorStatus(res, status) {
+    if (status !== "aberto" && status !== "solucionado") {
+        return res.status(400).json(errorHandler.handleError(400, "Tipo de status inválido", "tipoStatusInvalido", "Tipo de status inválido. Selecionar 'aberto' ou 'solucionado'."));
+    }
+
     const dados = casosRepository.listarCasosPorStatus(status);
 
     if (!dados || dados.length === 0) {
-        return next(new ApiError(404, "Caso não encontrado com esse status"));
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Caso não encontrado com esse status"));
     }
 
     return res.status(200).json(dados);
 }
 
-function listarPorAgenteEStatus(res, agente_id, status, next) {
+function listarPorAgenteEStatus(res, agente_id, status) {
     if (!agentesRepository.encontrarAgenteById(agente_id)) {
-        return next(new ApiError(404, "ID do agente informado não encontrado no sistema."));
+        return res.status(404).json(errorHandler.handleError(404, "ID do agente informado não encontrado no sistema.", "agenteNaoEncontrado", "ID do agente informado não encontrado no sistema."));
     }
 
     const dados = casosRepository.listarCasosPorAgenteEStatus(agente_id, status);
 
     if (!dados || dados.length === 0) {
-        return next(new ApiError(404, "Caso não encontrado com esse agente e status"));
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Caso não encontrado com esse agente e status"));
     }
 
     return res.status(200).json(dados);
 }
 
-function getAllCasos(req, res, next) {
-    try {
-        const { agente_id, status } = validarAgente_idEStatus.parse(req.query);
+function getAllCasos(req, res) {
+    const { agente_id, status } = req.query;
 
-        if (agente_id && status) {
-            return listarPorAgenteEStatus(res, agente_id, status);
-        }
-
-        else if (agente_id) {
-            return listarPorAgente(res, agente_id);
-        }
-
-        else if (status) {
-            return listarPorStatus(res, status);
-        }
-
-        const dados = casosRepository.findAll();
-
-        if (!dados) {
-            return next(new ApiError(404, "Nenhum caso registrado em nosso sistema."));
-        }
-
-        res.status(200).json(dados);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(400, "Parâmetros inválidos"))
-        }
-        next(error);   
+    if (agente_id && status) {
+        return listarPorAgenteEStatus(res, agente_id, status);
     }
+
+    else if (agente_id) {
+        return listarPorAgente(res, agente_id);
+    }
+
+    else if (status) {
+        return listarPorStatus(res, status);
+    }
+
+    const dados = casosRepository.findAll();
+
+    res.status(200).json(dados);
 }
 
-function getCaso(req, res, next) {
-    try {
-        const { id } = validarIDs.parse(req.params);
+function getCaso(req, res) {
+    const { id } = req.params;
+    const caso = casosRepository.findById(id);
 
-        const caso = casosRepository.findById(id);
-
-        if (!caso) {
-            return next(new ApiError(404, "Caso não encontrado."));
-        }
-
-        res.status(200).json(caso);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(404, "ID inválido"))
-        } 
-        next(error);         
+    if (!caso) {
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Caso não encontrado."));
     }
+
+    res.status(200).json(caso);
 }
 
-function postCaso(req, res, next) {
-    try {
-        const { titulo, descricao, status, agente_id } = validarDadosCasos.parse(req.body);
+function postCaso(req, res) {
+    const { titulo, descricao, status, agente_id } = req.body;
 
-        if (!agentesRepository.encontrarAgenteById(agente_id)) {
-            return next(new ApiError(404, "Agente informado não encontrado."));
-        }
-
-        const novoCaso = { titulo, descricao, status, agente_id };
-        const dados = casosRepository.adicionarCaso(novoCaso);
-
-        if(!dados) {
-            return next(new ApiError(404, "Agente não foi encontrado com esse id."));
-        }
-
-        res.status(201).json(dados);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(400, "Parâmetros inválidos"))
-        }
-        next(error); 
+    if (!titulo || !descricao || !status || !agente_id) {
+        return res.status(400).json(errorHandler.handleError(400, "Todos os campos são obrigatórios", "camposObrigatorios", "Todos os campos são obrigatórios."));
     }
+
+    if (status !== "aberto" && status !== "solucionado") {
+        return res.status(400).json(errorHandler.handleError(400, "Tipo de status inválido", "tipoStatusInvalido", "Tipo de status inválido. Selecionar 'aberto' ou 'solucionado'."));
+    }
+
+    if (!agentesRepository.encontrarAgenteById(agente_id)) {
+        return res.status(404).json(errorHandler.handleError(404, "Agente informado não encontrado", "agenteNaoEncontrado", "Agente informado não encontrado."));
+    }
+
+    const novoCaso = { titulo, descricao, status, agente_id };
+    const dados = casosRepository.adicionarCaso(novoCaso);
+
+    res.status(201).json(dados);
 }
 
-function putCaso(req, res, next) {
-    try {
-        let id;
-        try {
-            id = validarIDs.parse(req.params).id;
-        } catch (error) {
-            if (error instanceof z.ZodError) {
-                return next(new ApiError(404, "ID inválido"))
-            } 
-            return next(error); 
-        }
+function putCaso(req, res) {
+    const { id } = req.params;
+    const { id: idBody, titulo, descricao, status, agente_id } = req.body;
 
-        const { titulo, descricao, status, agente_id } = validarDadosCasos.parse(req.body);
-
-        if (!agentesRepository.encontrarAgenteById(agente_id)) {
-            return next(new ApiError(404, "Agente não encontrado. Verifique se o agente está registrado no sistema."));
-        }
-
-        const casoAtualizado = { titulo, descricao, status, agente_id };
-        const dados = casosRepository.atualizarCaso(id, casoAtualizado);
-
-        if (!dados) {
-            return next(new ApiError(404, "Caso não encontrado."));
-        }
-
-        res.status(200).json(dados);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(400, "Parâmetros inválidos"))            
-        }
-        next(error);   
+    if(idBody && idBody !== id) {
+        return res.status(400).json(errorHandler.handleError(400, "Alteração de ID não permitida", "idAlterado", "O campo 'id' não pode ser alterado."));
     }
+
+    if (!titulo || !descricao || !status || !agente_id) {
+        return res.status(400).json(errorHandler.handleError(400, "Todos os campos são obrigatórios", "camposObrigatorios", "Todos os campos são obrigatórios."));
+    }
+
+    if (status !== "aberto" && status !== "solucionado") {
+        return res.status(400).json(errorHandler.handleError(400, "Tipo de status inválido", "tipoStatusInvalido", "Tipo de status inválido. Selecionar 'aberto' ou 'solucionado'."));
+    }
+
+    if (!agentesRepository.encontrarAgenteById(agente_id)) {
+        return res.status(404).json(errorHandler.handleError(404, "Agente não encontrado", "agenteNaoEncontrado", "Agente não encontrado. Verifique se o agente está registrado no sistema."));
+    }
+
+    const casoAtualizado = { titulo, descricao, status, agente_id };
+    const dados = casosRepository.atualizarCaso(id, casoAtualizado);
+
+    if (!dados || dados.length === 0) {
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Caso não encontrado."));
+    }
+
+    res.status(200).json(dados);
 }
 
-function patchCaso(req, res, next) {
-    try {
-        let id;
-        try {
-            id = validarIDs.parse(req.params).id;            
-        } catch (error) {
-            if (error instanceof z.ZodError) {
-                return next(new ApiError(404, "ID inválido"))
-            } 
-            return next(error);              
-        }
+function patchCaso(req, res) {
+    const { id } = req.params;
+    const { id: idBody, titulo, descricao, status, agente_id } = req.body;
 
-        const { titulo, descricao, status, agente_id } = validarDadosParcialCasos.parse(req.body);
-
-        const casoAtualizado = { titulo, descricao, status, agente_id };
-        const dados = casosRepository.atualizarCaso(id, casoAtualizado);
-
-        if (!dados) {
-            return next(new ApiError(404, "Caso não encontrado."));
-        } 
-
-        res.status(200).json(dados);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(400, "Parâmetros inválidos"))            
-        }
-        next(error);          
+    if(idBody && idBody !== id) {
+        return res.status(400).json(errorHandler.handleError(400, "Alteração de ID não permitida", "idAlterado", "O campo 'id' não pode ser alterado."));
     }
+
+    if (!titulo && !descricao && !status && !agente_id) {
+        return res.status(400).json(errorHandler.handleError(400, "Um Campo Obrigatório", "camposObrigatorios", "Pelo menos um campo deve ser fornecido."));
+    }
+
+    if (status && status !== "aberto" && status !== "solucionado") {
+        return res.status(400).json(errorHandler.handleError(400, "Tipo de status inválido", "tipoStatusInvalido", "Tipo de status inválido. Selecionar 'aberto' ou 'solucionado'."));
+    }
+
+    if (agente_id && !agentesRepository.encontrarAgenteById(agente_id)) {
+        return res.status(404).json(errorHandler.handleError(404, "Agente não encontrado", "agenteNaoEncontrado", "Agente não encontrado. Verifique se o agente está registrado no sistema."));
+    }
+
+    const casoAtualizado = { titulo, descricao, status, agente_id };
+    const dados = casosRepository.atualizarParcialCaso(id, casoAtualizado);
+
+    if (!dados || dados.length === 0) {
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Caso não encontrado."));
+    } 
+
+    res.status(200).json(dados);
 }
 
-function deleteCaso(req, res, next) {
-    try {
-        const { id } = validarIDs.parse(req.params);
+function deleteCaso(req, res) {
+    const { id } = req.params;
+    const status = casosRepository.apagarCaso(id);
 
-        const status = casosRepository.apagarCaso(id);
-
-        if (!status) {
-            return next(new ApiError(404, "Caso não encontrado."));
-        }
-
-        res.status(204).send();        
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(404, "ID inválido"))
-        }
-        next(error);    
+    if (!status) {
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Caso não encontrado."));
     }
+
+    res.status(204).send();
 }
 
-function getAgenteDoCaso(req, res, next) {
-    try {
-        const { caso_id } = validarIDs.parse(req.params);    
+function getAgenteDoCaso(req, res) {
+    const { caso_id } = req.params;
 
-        if (!casosRepository.findById(caso_id)) {
-            return next(new ApiError(404, "ID do caso informado não encontrado."));
-        }
-
-        const dados = casosRepository.encontrarAgenteDoCaso(caso_id);
-
-        if (!dados) {
-            return next(new ApiError(404, "Agente não encontrado. Verifique se o agente está registrado no sistema."));
-        }
-
-        res.status(200).json(dados);        
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(404, "ID inválido"))
-        }
-        next(error);
+    if (!casosRepository.findById(caso_id)) {
+        return res.status(404).json(errorHandler.handleError(404, "ID do caso informado não encontrado", "casoNaoEncontrado", "ID do caso informado não encontrado."));
     }
+
+    const dados = casosRepository.encontrarAgenteDoCaso(caso_id);
+
+    if (!dados) {
+        return res.status(404).json(errorHandler.handleError(404, "Agente não encontrado", "agenteNaoEncontrado", "Agente não encontrado. Verifique se o agente está registrado no sistema."));
+    }
+
+    res.status(200).json(dados)
 }
 
-function getCasosPorString(req, res, next) {
-    try {
-        const { q } = validarString.parse(req.query);
+function getCasosPorString(req, res) {
+    const { q } = req.query;
 
-        const dados = casosRepository.encontrarCasoPorString(q);
-
-        if (!dados || dados.length === 0) {
-            return next(new ApiError(404, "Nenhum caso encontrado com a palavra fornecida."));
-        }
-
-        res.status(200).json(dados);        
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return next(new ApiError(400, "Parâmetros inválidos"))            
-        }
-        next(error);
+    if(!q) {
+        return res.status(400).json(errorHandler.handleError(400, "Parâmetro não encontrado", "parametroNaoEncontrado", "Verifique se está utilizando o parametro 'q' e se colocou alguma palavra para buscar."));
     }
+
+    const dados = casosRepository.encontrarCasoPorString(q);
+
+    if (!dados || dados.length === 0) {
+        return res.status(404).json(errorHandler.handleError(404, "Caso não encontrado", "casoNaoEncontrado", "Nenhum caso encontrado com a palavra fornecida."));
+    }
+
+    res.status(200).json(dados);
 }
 
 module.exports = {
